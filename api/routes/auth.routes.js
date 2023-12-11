c35aba300ec574bcb3fb384711327f5b47413e1b
Atharva Nagore <atharvanagore2004@gmail.com>
2023-12-11 22:41:45 +0530

    created signin api route

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { Signup } from '../controllers/auth.controller.js';
+import { Signup, signin } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/signup', Signup)
+router.post("/signin", signin);
 
 export default router;
