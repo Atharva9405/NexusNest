39aec5c3d481396f93eb3c294b2e9fc1ce6ed2db
Atharva Nagore <atharvanagore2004@gmail.com>
2023-12-12 22:48:19 +0530

    created profile page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,50 @@
+import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
 
 export default function Profile() {
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+
   return (
-    <div>
-      Profile
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+          alt="Profile"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          defaultValue={currentUser.username}
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          defaultValue={currentUser.email}
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="border p-3 rounded-lg"
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
     </div>
-  )
+  );
 }
