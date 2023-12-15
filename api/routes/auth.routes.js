b58ebe3b3dea1d7a9636d1d4489f18ac7cc93d3f
Atharva Nagore <atharvanagore2004@gmail.com>
2023-12-15 12:17:58 +0530

    added signout functionality

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { Signup, google, signin } from '../controllers/auth.controller.js';
+import { Signup, google, signOut, signin } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/signup', Signup)
 router.post("/signin", signin);
 router.post('/google',google)
+router.get('/signout', signOut)
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -106,6 +106,21 @@ export default function Profile() {
     }
   }
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -168,7 +183,7 @@ export default function Profile() {
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className='text-green-700 mt-5'>
