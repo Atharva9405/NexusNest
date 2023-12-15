2bd13c0810571e17858fb171b409938276309fa1
Atharva Nagore <atharvanagore2004@gmail.com>
2023-12-15 12:47:29 +0530

    created listing model and listing api route

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
-import userRouter from './routes/user.routes.js'
+import userRouter from './routes/user.routes.js';
+import listingRouter from './routes/listing.routes.js'
 
 const app = express();
 
@@ -29,7 +30,8 @@ app.listen(PORT, () => {
 });
 
 app.use('/api/user', userRouter)
-app.use("/api/auth", authRouter);
+app.use("/api/auth", authRouter);4
+app.use("/api/listing", listingRouter)
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
